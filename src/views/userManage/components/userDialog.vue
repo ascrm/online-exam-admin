0160e2a22baf1d2d87cbc8a3c6dce8af10ec86dd
ascrm <2339621373@qq.com>
2025-01-28 02:27:56 +0800

    用户管理的增删查改

---
+++ src/views/userManage/components/userDialog.vue
@@ -0,0 +1,38 @@
+<!-- @format -->
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const dialogFormVisible = ref(true)
+
+const form = ref({
+  username: '',
+})
+
+//打开对话框
+const open = () => {
+  dialogFormVisible.value = true
+}
+
+defineExpose({
+  open,
+})
+</script>
+
+<template>
+  <div>
+    <el-dialog v-model="dialogFormVisible" title="新增管理员" width="500">
+      <el-form v-model="form">
+        <el-form-item label="账号">
+          <el-input v-model="form.username" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false"> 确定 </el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
+</template>
---
+++ src/views/userManage/index.vue
@@ -0,0 +1,161 @@
+<!-- @format -->
+
+<template>
+  <div class="table-box">
+    <ProTable
+      ref="proTable"
+      :columns="columns"
+      :request-api="getTableList"
+      :init-param="initParam"
+      :data-callback="dataCallback"
+      @drag-sort="sortTable"
+    >
+      <!-- 表格 header 按钮 -->
+      <template #tableHeader="scope">
+        <el-button type="primary" :icon="CirclePlus" @click="openDialog">新增管理员</el-button>
+        <el-button type="primary" :icon="Download" plain @click="downloadFile">导出用户数据</el-button>
+        <el-button
+          type="danger"
+          :icon="Delete"
+          plain
+          :disabled="!scope.isSelected"
+          @click="batchDelete(scope.selectedListIds)"
+        >
+          批量删除用户
+        </el-button>
+      </template>
+
+      <!-- 表格操作 -->
+      <template #operation="scope">
+        <el-button type="primary" link :icon="Refresh" @click="resetPass(scope.row)">重置密码</el-button>
+        <el-button type="primary" link :icon="Delete" @click="deleteAccount(scope.row)">删除</el-button>
+      </template>
+    </ProTable>
+    <UserDialog ref="userDialogRef"></UserDialog>
+    <ImportExcel ref="dialogRef" />
+  </div>
+</template>
+
+<script setup lang="tsx" name="useProTable">
+import { ref, reactive } from 'vue'
+import { User } from '@/api/interface'
+import { useHandleData } from '@/hooks/useHandleData'
+import { useDownload } from '@/hooks/useDownload'
+import { ElMessage, ElMessageBox } from 'element-plus'
+import ProTable from '@/components/ProTable/index.vue'
+import ImportExcel from '@/components/ImportExcel/index.vue'
+import { ProTableInstance, ColumnProps } from '@/components/ProTable/interface'
+import { CirclePlus, Delete, Download, Refresh } from '@element-plus/icons-vue'
+import { getUserList, deleteUser, resetUserPassWord, exportUserInfo, BatchAddUser } from '@/api/modules/user'
+import UserDialog from './components/userDialog.vue'
+
+// ProTable 实例
+const proTable = ref<ProTableInstance>()
+
+// 如果表格需要初始化请求参数，直接定义传给 ProTable (之后每次请求都会自动带上该参数，此参数更改之后也会一直带上，改变此参数会自动刷新表格数据)
+const initParam = reactive({ type: 1 })
+
+// 或者直接去 hooks/useTable.ts 文件中把字段改为你后端对应的就行
+const dataCallback = (data: any) => {
+  return {
+    list: data.list,
+    total: data.total,
+  }
+}
+
+// 默认不做操作就直接在 ProTable 组件上绑定	:requestApi="getUserList"
+const getTableList = (params: any) => {
+  let newParams = JSON.parse(JSON.stringify(params))
+  newParams.createTime && (newParams.startTime = newParams.createTime[0])
+  newParams.createTime && (newParams.endTime = newParams.createTime[1])
+  delete newParams.createTime
+  return getUserList({ pageNum: newParams.pageNum, pageSize: newParams.pageSize })
+}
+
+// 表格配置项
+const columns = reactive<ColumnProps<User.ResUserList>[]>([
+  { type: 'selection', fixed: 'left', width: 70 },
+  { type: 'sort', label: 'Sort', width: 80 },
+  {
+    prop: 'username',
+    label: '用户账号',
+    search: { el: 'input', tooltip: '我是搜索提示' },
+  },
+  {
+    prop: 'role',
+    label: '角色',
+  },
+  {
+    prop: 'nickName',
+    label: '昵称',
+  },
+  {
+    prop: 'gender',
+    label: '性别',
+    search: { el: 'select', props: { filterable: true } },
+  },
+  { prop: 'email', label: '邮箱' },
+  { prop: 'phone', label: '电话' },
+  {
+    prop: 'createdAt',
+    label: '创建时间',
+    width: 180,
+    search: {
+      el: 'date-picker',
+      span: 2,
+      props: { type: 'datetimerange', valueFormat: 'YYYY-MM-DD HH:mm:ss' },
+      defaultValue: ['2022-11-12 11:35:00', '2022-12-12 11:35:00'],
+    },
+  },
+  { prop: 'operation', label: '操作', fixed: 'right', width: 330 },
+])
+
+// 表格拖拽排序
+const sortTable = ({ newIndex, oldIndex }: { newIndex?: number; oldIndex?: number }) => {
+  ElMessage.success('修改列表排序成功')
+}
+
+// 删除用户信息
+const deleteAccount = async (params: User.ResUserList) => {
+  await useHandleData(deleteUser, { id: [params.id] }, `删除【${params.username}】用户`)
+  proTable.value?.getTableList()
+}
+
+// 批量删除用户信息
+const batchDelete = async (id: string[]) => {
+  await useHandleData(deleteUser, { id }, '删除所选用户信息')
+  proTable.value?.clearSelection()
+  proTable.value?.getTableList()
+}
+
+// 重置用户密码
+const resetPass = async (params: User.ResUserList) => {
+  await useHandleData(resetUserPassWord, { id: params.id }, `重置【${params.username}】用户密码`)
+  proTable.value?.getTableList()
+}
+
+// 导出用户列表
+const downloadFile = async () => {
+  ElMessageBox.confirm('确认导出用户数据?', '温馨提示', { type: 'warning' }).then(() =>
+    useDownload(exportUserInfo, '用户列表', proTable.value?.searchParam),
+  )
+}
+
+// 批量添加用户
+const dialogRef = ref<InstanceType<typeof ImportExcel> | null>(null)
+const batchAdd = () => {
+  const params = {
+    title: '用户',
+    tempApi: exportUserInfo,
+    importApi: BatchAddUser,
+    getTableList: proTable.value?.getTableList,
+  }
+  dialogRef.value?.acceptParams(params)
+}
+
+//新增管理员
+const userDialogRef = ref<InstanceType<typeof UserDialog> | null>(null)
+const openDialog = () => {
+  userDialogRef.value?.open()
+}
+</script>
